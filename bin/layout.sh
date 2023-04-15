cd6733c84d85eba4a658a13998f859db34de59d7
tricarte <sonagirbas@gmail.com>
2023-04-15 15:01:11 +0300

    deletions

--- bin/layout.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# eval $(xdotool getmouselocation --shell)
-xdotool search --desktop 0 --name "Firefox" windowactivate
-xdotool key super 5
-# xdotool click 1
-# xdotool mousemove "$X" "$Y"
