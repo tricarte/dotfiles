31703abf537a1d8238f69b260315b41f93a72eda
tricarte <sonagirbas@gmail.com>
2025-10-04 13:37:21 +0300

    use mini-comment

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -7,3 +7,10 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "lf",
+--   group = vim.api.nvim_create_augroup("commentLf", {}),
+--   callback = function()
+--     vim.bo.commentstring = "# %s"
+--   end,
+-- })
